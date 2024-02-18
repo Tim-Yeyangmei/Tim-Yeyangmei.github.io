af23d6e7ebde061d54413375a69a0780b3a0d09d
Yeyangmei <122388649@qq.com>
2024-02-18 23:46:09 +0800

    更新github账号

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Yeyangmei', // Usually your GitHub org/user name.
+  organizationName: 'Tim-Yeyangmei', // Usually your GitHub org/user name.
   projectName: 'tim-yeyangmei.github.io', // Usually your repo name.
   deploymentBranch:"gh-pages",
   onBrokenLinks: 'throw',
